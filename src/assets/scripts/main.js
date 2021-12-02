135406dcbac2a19b77e8a5e478afa1ea1271700f
Pablo Ros <pablo.ros.flores@gmail.com>
2021-12-02 01:21:07 +0100

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-relative-packages */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+// Import all plugins
+import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle";
